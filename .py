b17295136fd0dc7cfaed9133403e28b3b8be84cd
Mackoor <maciekkin13@gmail.com>
2026-05-29 20:16:37 +0200

    Create .py

---
+++ .py
@@ -0,0 +1,8 @@
+            elif inp == "g":
+                randint(0,6)
+                if randint == 0:
+                    print("\tdostałes quest zbierania ziół")
+                    inp = input("\ta akceptujesz \to odrzucasz")
+                    if inp == "a":
+                        if print("o"):
+                            break
